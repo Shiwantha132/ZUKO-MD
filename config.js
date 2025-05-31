9050360fcd3438c3bbc18441680c3bf800a61252
Shiwantha132 <shiwantha@wgtapps.com>
2025-05-31 21:02:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/vn037q.j
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "ZUKO-MD",
+BOT_NAME: process.env.BOT_NAME || "shiwa md",
 // add bot namw here for menu
 STICKER_NAME: process.env.STICKER_NAME || "ZUKO-MD",
 // type sticker pack name 
@@ -37,7 +37,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "2349079XXXX",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "0775921987",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "ZÜKØ Tᴇᴄʜ",
 // add bot owner name
